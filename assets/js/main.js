89eeb63ee5620e026a70e2312aa8511052c1979f
Andre J. Reid <axr490@miami.edu>
2019-12-30 15:53:45 -0500

    added basic code for moving the drawn cube

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ let gameArea = {
         this.canvas.css("border", "2px solid black");
         this.context = this.canvas[0].getContext("2d");
         $(document.body).prepend(this.canvas);
-        this.interval = setInterval(updateGameArea, 1000);
+        this.interval = setInterval(updateGameArea, 20);
     },
     clear: function() {
         this.context.clearRect(0, 0, this.canvas.width(), this.canvas.height());
@@ -17,11 +17,13 @@ let gameArea = {
 $(document).ready(function () {
      gameArea.start();
      gamePiece = new component(10, 10, "darkgreen", 25, 25);
+     ConfigureButtons();
 })
 
 function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
+    this.speedX = 0;
     this.color = color;
     this.x = x;
     this.y = y;
@@ -31,9 +33,35 @@ function component(width, height, color, x, y) {
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+
+    this.newPos = function() {
+        this.x += this.speedX;
+    }
 }
 
 function updateGameArea() {
     gameArea.clear();
+    gamePiece.newPos();
     gamePiece.update();
+}
+
+function ConfigureButtons() {
+    document.onkeydown = function(event) {
+        switch (event.code) {
+            case "ArrowLeft":
+                MoveLeft();
+                break;
+            case "ArrowRight":
+                MoveRight();
+                break;
+        }
+    }
+}
+
+function MoveLeft() {
+    gamePiece.speedX -= 1;
+}
+
+function MoveRight() {
+    gamePiece.speedX += 1;
 }
