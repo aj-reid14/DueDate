eeca6a5f6567af80a4cf30b54bb6647bbc1c6d1b
Andre J. Reid <axr490@miami.edu>
2019-12-30 15:35:46 -0500

    now drawing to the canvas using canvas drawing methods

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,17 +7,17 @@ let gameArea = {
         this.canvas.css("border", "2px solid black");
         this.context = this.canvas[0].getContext("2d");
         $(document.body).prepend(this.canvas);
+        this.interval = setInterval(updateGameArea, 1000);
+    },
+    clear: function() {
+        this.context.clearRect(0, 0, this.canvas.width(), this.canvas.height());
     }
 }
 
 $(document).ready(function () {
-     alert("ready");
      gameArea.start();
+     gamePiece = new component(10, 10, "darkgreen", 25, 25);
 })
-
-function StartGame() {
-    gamePiece = new component(30, 30, "red", 10, 120);
-}
 
 function component(width, height, color, x, y) {
     this.width = width;
@@ -25,4 +25,15 @@ function component(width, height, color, x, y) {
     this.color = color;
     this.x = x;
     this.y = y;
+
+    this.update = function() {
+        ctx = gameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+function updateGameArea() {
+    gameArea.clear();
+    gamePiece.update();
 }
