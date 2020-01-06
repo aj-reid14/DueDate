a58fdc2b3985dd9df9bead85cd2e3dfb544a2056
ajreid <axr490@miami.edu>
2020-01-05 22:51:12 -0500

    added code for updating gif and moving image when key is pressed

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -1,18 +1,20 @@
 $(document).ready(function () {
     CreateComponents();
+    ConfigureButtons();
 })
 
 function CreateComponents() {
 
     // Static Box (Red)
-    player = new component(50, 85, "./assets/images/laptop/idle.gif", 25, 25, "image");
+    player = new component("player", 100, 125, "./assets/images/laptop/idle.gif", 25, 25, "image");
 }
 
-function component(width, height, color, x, y, type) {
+function component(id, width, height, color, x, y, type) {
+    this.id = id;
     this.width = width;
     this.height = height;
     this.x = x;
-    this.speedX = 0;
+    this.speedX = 4;
     this.y = y;
     this.speedY = 0;
 
@@ -20,7 +22,73 @@ function component(width, height, color, x, y, type) {
     else { this.color = color; }
 
     if (type === "image") {
-        this.element = $(`<img src=${this.src} width=${width} height=${height}>`);
+        this.element = $(`<img id='${this.id}' src='${this.src}' width=${width} height=${height}>`);
+        this.element.css({position: "absolute"});
+        this.element.css({top: 0, left: 0});
         $("#display").append(this.element);
+    }
+
+    this.move = function (direction) {
+        Move(this.id, direction);
+    }
+}
+
+function Move(id, direction) {
+
+    switch (direction) {
+        case "left": {
+            let currLeft = parseInt($(`#${id}`).css("left"), 10);
+
+            if (currLeft > 0) {
+                $(`#${id}`).css("left", currLeft - player.speedX);
+            }
+        }
+            break;
+        case "right": {
+            let currLeft = parseInt($(`#${id}`).css("left"), 10);
+            let currRight = $(`#${id}`)[0].getBoundingClientRect().right;
+            let maxRight = $("#display")[0].getBoundingClientRect().right;
+
+            if (currRight < maxRight) {
+                $(`#${id}`).css("left", currLeft + player.speedX);
+            }
+        }
+            break;
+        case "up":
+            break;
+        case "down":
+            break;
+    }
+}
+
+function ConfigureButtons() {
+    document.onkeydown = function (event) {
+        switch (event.code) {
+            case "ArrowLeft":
+                player.src = "./assets/images/laptop/walk_left.gif";
+                player.move("left");
+                break;
+            case "ArrowRight":
+                player.src = "./assets/images/laptop/walk_right.gif";
+                player.move("right");
+                break;
+        }
+
+        if (player.element.attr("src") !== player.src) {
+            player.element.attr("src", player.src);
+        }
+    }
+
+    document.onkeyup = function(event) {
+        switch (event.code) {
+            case "ArrowLeft":
+            case "ArrowRight":
+                player.src = "./assets/images/laptop/idle.gif";
+                break;
+        }
+
+        if (player.element.attr("src") !== player.src) {
+            player.element.attr("src", player.src);
+        }
     }
 }
