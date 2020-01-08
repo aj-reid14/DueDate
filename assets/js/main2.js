18aa40718960a0c2354225f8a6e61440de314210
ajreid <axr490@miami.edu>
2020-01-07 23:37:09 -0500

    moved shootas to left side, made 'display' responsive to screen size, and prevented player from walking below shootas

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -10,26 +10,32 @@ $(document).ready(function () {
 function CreateComponents() {
 
     // Static Box (Red)
-    player = new component("player", "auto", "auto", "./assets/images/laptop/idle.gif", 25, 25, "image");
-    
-    let maxPlatformLeft = $("#display")[0].getBoundingClientRect().right + 5;
+    let playerX = $("#display")[0].getBoundingClientRect().width / 2;
+    player = new component("player", "auto", "auto", "./assets/images/laptop/idle.gif", playerX, 0, "image");
+
+    let platformWidth = 150;
+    let maxPlatformLeft = $("#display")[0].getBoundingClientRect().left;
+    console.log(maxPlatformLeft);
 
     // Create the Shootas
     let shootaDistance = 130;
-    let shootaLeft = maxPlatformLeft - 350;
-    
+    let shootaLeft = 25;
+
     let jonboy = new component("jonboy", 100, 100, "./assets/images/shootas/jonboy/idle.gif", shootaLeft, 0, "image");
     jonboy.element.css("z-index", 1);
+    jonboy.element.css("transform", "scaleX(-1)");
     console.log(`${jonboy.id} | Top: ${jonboy.element.css("top")} | Left: ${jonboy.element.css("left")}`);
     shootas.push(jonboy);
 
     let derry = new component("derry", 100, 100, "./assets/images/shootas/derry/idle.gif", shootaLeft, 0 + shootaDistance, "image");
     derry.element.css("z-index", 1);
+    derry.element.css("transform", "scaleX(-1)");
     console.log(`${derry.id} | Top: ${derry.element.css("top")} | Left: ${derry.element.css("left")}`);
     shootas.push(derry);
 
     let charlington = new component("charlington", 100, 100, "./assets/images/shootas/charlington/idle.gif", shootaLeft, 0 + (shootaDistance * 2), "image");
     charlington.element.css("z-index", 1);
+    charlington.element.css("transform", "scaleX(-1)");
     console.log(`${charlington.id} | Top: ${charlington.element.css("top")} | Left: ${charlington.element.css("left")}`);
     shootas.push(charlington);
 
@@ -38,10 +44,11 @@ function CreateComponents() {
     let platformTop = 100;
 
     for (let i = 0; i < 3; i++) {
-        let platform = new component(`platform${i}`, 200, 25, "darkgrey", 0, 0, "none");
-        platform.element.css({top: platformTop, left: maxPlatformLeft - (platform.width * 2)});
+        let platform = new component(`platform${i}`, platformWidth, 25, "darkgrey", 0, platformTop, "none");
         platform.element.addClass("platform");
         platforms.push(platform);
+        console.log(`Platform ${i} [width]: ` + $(`#platform${i}`)[0].getBoundingClientRect().width);
+
         platformTop += shootaDistance;
     }
 }
@@ -58,25 +65,31 @@ function component(id, width, height, color, x, y, type) {
     if (type === "image") {
         this.src = color;
         this.element = $(`<img id='${this.id}' src='${this.src}'>`);
-        this.element.css({position: "absolute", width: this.width, height: this.height});
-        this.element.css({top: y, left: x});
-        $("#display").append(this.element);
+        this.element.css({ position: "absolute", width: this.width, height: this.height });
+        this.element.css({ top: y, left: x });
 
         if (this.id === "player") {
-            let displayBottom = $("#display")[0].getBoundingClientRect().bottom;
-            let playerHeight = 89 + 20;
-            this.element.css({top: displayBottom - playerHeight});
+            let displayBottom = $("#display")[0].getBoundingClientRect().bottom - 20;
+            let playerHeight = 89;
+            this.element.css({ margin: "0px", top: displayBottom - playerHeight });
+
+            $(window).resize(function () {
+                if ($("#player")[0].getBoundingClientRect().left > $("#display")[0].getBoundingClientRect().right) {
+                    $("#player").css({ left: $("#display")[0].getBoundingClientRect().width - 150 });
+                }
+
+            })
         }
 
     } else {
         this.element = $(`<div id='${this.id}'>`);
         this.color = color;
-        this.element.css({position: "absolute", width: this.width, height: this.height});
+        this.element.css({ position: "absolute", width: this.width, height: this.height, top: this.y, left: this.x });
         this.element.css("background-color", color);
         this.element.css("z-index", 10);
-        $("#display").append(this.element);
     }
 
+    $("#display").append(this.element);
     this.element = $(`#${this.id}`);
 
     this.move = function (direction) {
@@ -89,8 +102,9 @@ function Move(id, direction) {
     switch (direction) {
         case "left": {
             let currLeft = parseInt($(`#${id}`).css("left"), 10);
+            let maxLeft = platforms[0].width;
 
-            if (currLeft > 0) {
+            if (currLeft > maxLeft) {
                 $(`#${id}`).css("left", currLeft - player.speedX);
             }
         }
@@ -104,10 +118,6 @@ function Move(id, direction) {
                 $(`#${id}`).css("left", currLeft + player.speedX);
             }
         }
-            break;
-        case "up":
-            break;
-        case "down":
             break;
     }
 }
@@ -135,7 +145,7 @@ function ConfigureButtons() {
         }
     }
 
-    document.onkeyup = function(event) {
+    document.onkeyup = function (event) {
         switch (event.code) {
             case "ArrowLeft":
             case "ArrowRight":
@@ -152,7 +162,7 @@ function ConfigureButtons() {
 function CreateDaBoom(shoota) {
 
     let boomTop = shoota.y + (shoota.height * 0.35);
-    let boomLeft = shoota.x;
+    let boomLeft = shoota.x + shoota.width;
     let daBoomID = `daBoom-${shoota.id}`;
 
     let daBoom = $(`<img id='${daBoomID}' src='./assets/images/gun_boom.gif'>`);
@@ -160,13 +170,13 @@ function CreateDaBoom(shoota) {
         display: "none",
         position: "absolute",
         top: boomTop,
-        left: boomLeft - 40,
+        left: boomLeft - 10,
         width: "50px",
         height: "50px",
-        transform: "rotate(-90deg)"
+        transform: "rotate(90deg)"
     });
     $("#display").append(daBoom);
-    $(`#${daBoomID}`).fadeIn(150, function() {
+    $(`#${daBoomID}`).fadeIn(150, function () {
         setTimeout(KillDaBoom, 200, daBoomID);
     });
 }
